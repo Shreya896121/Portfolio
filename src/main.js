4b239945ae721f1cf968f0e9521f4143f2dd1a61
Shreya <shakyash12@gmail.com>
2026-05-09 22:06:26 +0545

    first commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+console.log("Portfolio loaded successfully!");
+
+// You can add more interactivity here later
+document.querySelector('.btn').addEventListener('click', () => {
+  console.log("View my work button clicked!");
+  // Add scroll logic or navigation here
+});
